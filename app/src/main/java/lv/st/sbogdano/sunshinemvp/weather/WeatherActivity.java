c98175d711e9215eba90ff6463209d8d558e797e
SergejsBogdanovs <bogdanov.sergej@gmail.com>
2017-12-06 10:29:03 +0200

    Added navigation drawer mockup

--- app/src/main/java/lv/st/sbogdano/sunshinemvp/weather/WeatherActivity.java
+++ app/src/main/java/lv/st/sbogdano/sunshinemvp/weather/WeatherActivity.java
@@ -1,10 +1,29 @@
 package lv.st.sbogdano.sunshinemvp.weather;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.NavigationView.OnNavigationItemSelectedListener;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import lv.st.sbogdano.sunshinemvp.R;
 
 public class WeatherActivity extends AppCompatActivity {
+
+  @BindView(R.id.toolbar)
+  Toolbar mToolbar;
+
+  @BindView(R.id.drawer_layout)
+  DrawerLayout mDrawerLayout;
+
+  @BindView(R.id.nav_view)
+  NavigationView mNavView;
 
   private WeatherPresenter mWeatherPresenter;
 
@@ -12,7 +31,50 @@ public class WeatherActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_weather);
+    ButterKnife.bind(this);
 
+    // Set up the Toolbar
+    setSupportActionBar(mToolbar);
+    ActionBar ab = getSupportActionBar();
+    ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+    ab.setDisplayHomeAsUpEnabled(true);
 
+    // Set up the Navigation Drawer.
+    mDrawerLayout.setStatusBarBackground(R.color.colorPrimaryDark);
+    if (mNavView != null) {
+      setupDrawerContent(mNavView);
+    }
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case android.R.id.home:
+        mDrawerLayout.openDrawer(GravityCompat.START);
+        return true;
+    }
+    return super.onOptionsItemSelected(item);
+  }
+
+  private void setupDrawerContent(NavigationView navView) {
+    navView.setNavigationItemSelectedListener(item -> {
+      switch (item.getItemId()) {
+        case R.id.settings:
+          //TODO launch settings activity
+          break;
+        case R.id.item_celsius:
+          //TODO implement Celsius
+          break;
+        case R.id.item_kelvin:
+          // TODO implement Kelvin
+        default:
+          break;
+      }
+
+      // Close the navigation drawer when an item is selected.
+      item.setChecked(true);
+      mDrawerLayout.closeDrawers();
+      return true;
+    });
   }
 }
